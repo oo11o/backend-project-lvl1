1d6880731c86f86e4bb5f1a0511eae44142d98c0
Vlad Imir <ymyr.vlad@gmail.com>
2022-05-11 15:54:51 +0300

    fix change start game

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const template = {
-  welcome: 'Welcome to the Brain Games',
+  welcome: 'Welcome to the Brain Games!',
   question: 'May I have your name?',
 };
 
 console.log(template.welcome);
-const name = readlineSync.question(`${template.question} `);
-console.log(`Hello, ${name}!`);
-
 export default name;
