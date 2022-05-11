afc443178d320525fcb58fcc42604a8ddb33008d
Vlad Imir <ymyr.vlad@gmail.com>
2022-05-11 15:35:30 +0300

    refactoring base file

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const template = {
-    welcome: 'Welcome to the Brain Games',
-    question: 'May I have your name?'
-}
+  welcome: 'Welcome to the Brain Games',
+  question: 'May I have your name?',
+};
 
 console.log(template.welcome);
 const name = readlineSync.question(`${template.question} `);
--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
-import name from "./cli.js";
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+import name from './cli.js';
 
 const getRandom = (count = 100) => Math.floor(Math.random() * count);
 
 const template = {
-    rulesEvenGame: 'Answer "yes" if the number is even, otherwise answer "no".',
-    rulesCalcGame: 'What is the result of the expression?',
-    questionQuiz: 'Question: ',
-    answer: 'Your answer: ',
-    answerWrong: 'is wrong answer ;(. Correct answer was',
-    answerCorrect: 'Correct!',
-    gameDone: 'Congratulations,',
-    gameFail: 'Next time, bro!',
+  rulesEvenGame: 'Answer "yes" if the number is even, otherwise answer "no".',
+  rulesCalcGame: 'What is the result of the expression?',
+  rulesGcdGame: 'Find the greatest common divisor of given numbers.',
+  rulesProgressive: 'What number is missing in the progression?',
+  rulesPrime: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+  questionQuiz: 'Question: ',
+  answer: 'Your answer: ',
+  answerWrong: 'is wrong answer ;(. Correct answer was',
+  answerWrongGcd: 'Let\'s try again, Sam!',
+  answerCorrect: 'Correct!',
+  gameDone: 'Congratulations,',
+  gameFail: 'Let\'s try again,',
 };
 
 const getResultGame = (countCorrectAnswer, name) => {
-    const result = countCorrectAnswer === 3
-        ? `${template.gameDone} ${name}!`
-        : template.gameFail;
-    console.log(result);
-}
+  const result = countCorrectAnswer === 3
+    ? `${template.gameDone} ${name}!`
+    : `${template.gameFail} ${name}!`;
+  console.log(result);
+};
 
-const isCorrect = (correctAnswer, answerUser) => {
-    if (correctAnswer === answerUser) {
-        console.log(template.answerCorrect);
-        return true;
-    }
-    console.log(`'${answerUser}' ${template.answerWrong} '${correctAnswer}'`);
-    return false;
-}
+const isCorrect = (answerUser, correctAnswer) => {
+  if (correctAnswer === answerUser) {
+    console.log(template.answerCorrect);
+    return true;
+  }
+  console.log(`'${answerUser}' ${template.answerWrong} '${correctAnswer}'`);
+  return false;
+};
 
-export { getRandom, getResultGame, isCorrect, template }
+export {
+  getRandom, getResultGame, isCorrect, template,
+};
